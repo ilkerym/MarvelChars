19ba1b8aac255ed43a4db30a625535ef3f223ef5
ilker <ilker.memisoglu@icloud.com>
2022-02-22 05:40:43 +0300

    list characters and photos on main page

--- MarvelChars/CharactersViewController.swift
+++ MarvelChars/CharactersViewController.swift
@@ -10,104 +10,119 @@ import Alamofire
 import CryptoKit
 
 struct CharInfo {
-    var charId = [Int]()
-    var charName = [String]()
-    var charImage = [Image]()
+    
+    var charId : Int
+    var charName : String
+    var charImage : Image
 }
 
 class CharactersViewController: UIViewController {
-   
-    var marvelChars = CharInfo()
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var displayedCharsInfo = [CharInfo]()
+    var marvelChars = [Character]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        getAllChars()
+    }
+    
+    let url = "https://gateway.marvel.com:443/v1/public/characters"
+    
+    func MD5(data: String) -> String {
+        let hash = Insecure.MD5.hash(data: data.data(using: .utf8) ?? Data())
+        return hash.map{String(format: "%02hhx", $0)}.joined()
         
+    }
+    
+    func parameters (limit: Int = 100, offset: Int = 0) -> [String: Any] {
         let apiKey = "6bc760706a50feb51400ffff42782383"
         let privateKey = "629391ddc039d9f9615e10b323fb22984f145016"
         let ts = String(Date().timeIntervalSince1970)
         let hash = MD5(data:"\(ts)\(privateKey)\(apiKey)")
-       // let baseURL = "https://gateway.marvel.com:443/v1/public/characters"
-        let url = "https://gateway.marvel.com:443/v1/public/characters?ts=\(ts)&apikey=\(apiKey)&hash=\(hash)"
-        //let parameters: Parameters = ["apikey": apiKey,"ts": ts,"hash": hash]
-        func MD5(data: String) -> String {
-            let hash = Insecure.MD5.hash(data: data.data(using: .utf8) ?? Data())
-            return hash.map{String(format: "%02hhx", $0)}.joined()
-            
+        return  ["apikey":apiKey,"ts": ts,"hash": hash,"limit": limit,"offset": offset]
+    }
+    func getAllChars() {
+        AF.request(url, parameters: parameters(limit: 1, offset: 0)).validate().responseDecodable(of: CharacterDataWrapper.self) { (response) in
+            guard let numberOfTotalChar = response.value?.data?.total
+            else {return}
+            self.getCharacters(totalCharacters: numberOfTotalChar)
         }
+    }
+    func getCharacters(totalCharacters: Int){
+        var offset = 0
+        var requestedCharacters = 0
         
-        AF.request(url).responseDecodable(of: CharacterDataWrapper.self) { response in
-            if let error = response.error {
-                print(error)
-            } else {
-                if let jsonData = response.value{
-                    
-                    guard let data = jsonData.data?.results else {return print(response.error!)}
-                    
-//                    for (index, item) in data.enumerated() {
-//
-//                        self.marvelChars.charName.append(item.name!)
-//                        self.marvelChars.charId.append(item.id!)
-//                        self.marvelChars.charImage.append(item.thumbnail!)
-//
-//                    }
-                    print(data.count)
+//        while (requestedCharacters <= totalCharacters) {
+            AF.request(url, parameters: parameters(limit: 30, offset: offset)).validate().responseDecodable(of: CharacterDataWrapper.self) { (response) in
+                guard let apiResponse = response.value?.data?.results
+                else {return}
                 
-            }
+                self.marvelChars.append(contentsOf: apiResponse)
+                
+                for (_,item) in self.marvelChars.enumerated() {
+                    
+                    var data = CharInfo(charId: item.id!, charName: item.name!, charImage: item.thumbnail!)
+                    
+                    self.displayedCharsInfo.append(data)
+                    
+                    print(data.charImage.url)
+                    
+                }
+             
+                self.tableView.reloadData()
+                
+//            }
+//            offset += 100
+//            requestedCharacters += 100
             
+           
         }
-        
-        
-       
+
     }
     
+}
+//MARK: - TV Data Source Methods
+
+extension CharactersViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        return displayedCharsInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath) as! CharacterTableViewCell
+        
+        cell.charNameLabel.text = displayedCharsInfo[indexPath.row].charName
+        
+        guard let imageURL = displayedCharsInfo[indexPath.row].charImage.url else { return cell }
+        
+        cell.charImageView.load(url: imageURL)
+        
+        return cell
+        
+    }
+    
+    
+
 }
 
 
 
+//MARK: - TV Delegate Methods
 
-
-//                    let decoder = JSONDecoder()
-//                    guard let json = try? decoder.decode(CharacterDataWrapper.self, from: response.data!) else { print("Unable to parse JSON"); return }
-
-
-//        func performRequest(with urlString: String) {
-//            if let url = URL(string: urlString ) {
-//                let session = URLSession(configuration: .default)
-//                let task = session.dataTask(with: url) { (data, response, error) in
-//                    if error != nil {
-//                        print(error)
-//                        return
-//                    }
-//                    if let safeData = data {
-//                        if let character = self.parseJSON(safeData) {
-//                            print(character.id)
-//                            print(character.name)
-//                        }
-//                    }
-//                }
-//                task.resume()
-//            }
-//        }
-//
-//        performRequest(with: url)
-
-
-//    func parseJSON(_ jsonData: Data) -> CharInfo? {
-//        let decoder = JSONDecoder()
-//        do {
-//            let decodedData = try decoder.decode(CharacterDataWrapper.self, from: jsonData)
-//            let id = decodedData.data?.results![0].id
-//            let name = (decodedData.data?.results![0].name)!
-//            let charImage = decodedData.data?.results![0].thumbnail?.url
-//            let char = CharInfo(id: id!, name: name, charImage: charImage!)
-//
-//            return char
-//
-//        } catch {
-//            print(error)
-//            return nil
-//        }
-//
+extension CharactersViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        print(displayedCharsInfo[indexPath.row].charId)
+        return indexPath
     }
+    
+    
+}
+
